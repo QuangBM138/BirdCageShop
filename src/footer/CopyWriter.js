d52ad4b5826d28f9f9a4c1543dc51e55e64dc1d8
Dat Le Tien <letiendat1442001@gmail.com>
2023-09-28 07:30:59 +0700

    push header

---
+++ src/footer/CopyWriter.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+const CopyWriter = () => {
+  return (
+    <div className="container">
+      <p>2023 Culi Đánh Thuê &copy;</p>
+    </div>
+  );
+};
+
+export default CopyWriter;
