5bd40f580957aedb4ff9439b6e3cba1e93369859
Trang <ngotrang2508.forwork@gmail.com>
2023-09-30 13:55:12 +0700

    detail

--- src/components/productDetail/reviews/Reviews.js
+++ src/components/productDetail/reviews/Reviews.js
@@ -18,7 +18,7 @@ export default function Reviews() {
 
     return (
         <section className="text-gray-600 body-font overflow-hidden" >
-            <div className="container px-0 pb-12 sm:px-10 py-0 sm:py-18 mx-auto">
+            <div className=" px-0 pb-12 sm:px-10 py-0 sm:py-18 mx-auto">
                 <div className='container_showandreview'>
                     <div className='review_header'>
                         <h2 className='h2_ny'>
