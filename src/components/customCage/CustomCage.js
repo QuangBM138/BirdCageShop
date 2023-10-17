32cc9e13b85ff967a69a4762d7573934f48115bd
thasine2k1 <thaihoang03122001@gmail.com>
2023-10-17 12:48:54 +0700

    fix bug input value

--- src/components/customCage/CustomCage.js
+++ src/components/customCage/CustomCage.js
@@ -7,101 +7,176 @@ import Base from "./Base";
 
 export default function CustomCage() {
   const [inputValues, setInputValues] = useState({
-    cao: "",
-    rong: "",
-    dai: "",
+    height: "",
+    width: "",
+    length: "",
   });
   const [errors, setErrors] = useState({
-    cao: "",
-    rong: "",
-    dai: "",
+    height: "",
+    width: "",
+    length: "",
   });
   const isInputValid =
-    inputValues.cao !== "" && inputValues.rong !== "" && inputValues.dai !== "";
+    inputValues.height !== "" &&
+    inputValues.width !== "" &&
+    inputValues.length !== "" &&
+    inputValues.height >= 30 &&
+    inputValues.height <= 100 &&
+    inputValues.width >= 30 &&
+    inputValues.width <= 100 &&
+    inputValues.length >= 30 &&
+    inputValues.length <= 100 &&
+    inputValues.height < inputValues.length;
 
   const handleInputChange = (fieldName, value) => {
-    if (value < 30 || value > 100) {
-      setErrors((prevErrors) => ({
-        ...prevErrors,
-        [fieldName]: "Giá trị không hợp lệ. Vui lòng nhập từ 30 đến 100.",
-      }));
-    } else {
-      setErrors((prevErrors) => ({
-        ...prevErrors,
-        [fieldName]: "",
-      }));
+    const newInputValues = {
+      ...inputValues,
+      [fieldName]: value,
+    };
+    setInputValues(newInputValues);
+
+    if (fieldName === "height") {
+      if (value < 30 || value > 100) {
+        setErrors((prevErrors) => ({
+          ...prevErrors,
+          [fieldName]: "Giá trị không hợp lệ. Vui lòng nhập từ 30 đến 100.",
+        }));
+      } else {
+        setErrors((prevErrors) => ({
+          ...prevErrors,
+          [fieldName]: "",
+        }));
+      }
     }
 
-    setInputValues((prevInputValues) => ({
-      ...prevInputValues,
-      [fieldName]: value,
-    }));
+    if (fieldName === "width") {
+      if (value < 30 || value > 100) {
+        setErrors((prevErrors) => ({
+          ...prevErrors,
+          [fieldName]: "Giá trị không hợp lệ. Vui lòng nhập từ 30 đến 100.",
+        }));
+      } else if (value >= newInputValues.length) {
+        setErrors((prevErrors) => ({
+          ...prevErrors,
+          [fieldName]: "Width phải nhỏ hơn Length.",
+        }));
+      } else {
+        setErrors((prevErrors) => ({
+          ...prevErrors,
+          [fieldName]: "",
+        }));
+      }
+    }
+    if (fieldName === "length") {
+      if (value < 30 || value > 100) {
+        setErrors((prevErrors) => ({
+          ...prevErrors,
+          [fieldName]: "Giá trị không hợp lệ. Vui lòng nhập từ 30 đến 100.",
+        }));
+      } else {
+        setErrors((prevErrors) => ({
+          ...prevErrors,
+          [fieldName]: "",
+        }));
+        if (newInputValues.width >= value) {
+          setErrors((prevErrors) => ({
+            ...prevErrors,
+            width: "Width phải nhỏ hơn Length.",
+          }));
+        } else {
+          setErrors((prevErrors) => ({
+            ...prevErrors,
+            width: "",
+          }));
+        }
+      }
+    }
   };
+
   return (
     <div className="w-[90%] m-auto mt-5">
       <div className="size">
         <div>Size:</div>
         <div className="size_chilren">
-          Cao
+          Height
           <input
             className="ip_custom_cage"
-            value={inputValues.cao}
+            value={inputValues.height}
             type="number"
             min={30}
             max={100}
             onChange={(e) =>
-              handleInputChange("cao", parseInt(e.target.value, 10))
+              handleInputChange("height", parseInt(e.target.value, 10))
             }
           />
-          {errors.cao && <div className="error-message">{errors.cao}</div>}
+          {errors.height && (
+            <div className="error-message">{errors.height}</div>
+          )}
         </div>
         <div className="size_chilren">
-          Rộng
+          Width
           <input
             className="ip_custom_cage"
             type="number"
             min={30}
             max={100}
-            value={inputValues.rong}
+            value={inputValues.width}
             onChange={(e) =>
-              handleInputChange("rong", parseInt(e.target.value, 10))
+              handleInputChange("width", parseInt(e.target.value, 10))
             }
           />
-          {errors.rong && <div className="error-message">{errors.rong}</div>}
+          {errors.width && <div className="error-message">{errors.width}</div>}
         </div>
         <div className="size_chilren">
-          Dài
+          Length
           <input
             className="ip_custom_cage"
             type="number"
             min={30}
             max={100}
-            value={inputValues.dai}
+            value={inputValues.length}
             onChange={(e) =>
-              handleInputChange("dai", parseInt(e.target.value, 10))
+              handleInputChange("length", parseInt(e.target.value, 10))
             }
           />
-          {errors.dai && <div className="error-message">{errors.dai}</div>}
+          {errors.length && (
+            <div className="error-message">{errors.length}</div>
+          )}
         </div>
       </div>
-      <Roof isDisabled={!isInputValid} />
-      <Spoke isDisabled={!isInputValid} />
-      <Door isDisabled={!isInputValid} />
-      <Base isDisabled={!isInputValid} />
+      <div className="flex">
+        <div>
+          <Roof isDisabled={!isInputValid} />
+          <Spoke isDisabled={!isInputValid} />
+          <Door isDisabled={!isInputValid} />
+          <Base isDisabled={!isInputValid} />
+        </div>
+        <div className="flex flex-col justify-center w-[300px] h-[200px] mt-3 ml-10 items-center">
+          <div>
+            <div>
+              <text>Length, width, height(min: 30, max: 100)</text>
+            </div>
+            <div>Height &lt; Length</div>
+            <div>Spoke: min 30, max 100</div>
+            <div>Door &lt;= 4</div>
+          </div>
+        </div>
+      </div>
+
       <div className="flex mt-5">
         <img
           className="w-[400px] rounded-lg mx-5"
           src="https://c8.alamy.com/comp/2A424WA/ornate-bird-cage-inspired-roof-design-on-grand-mansion-rooftop-2A424WA.jpg"
         />
-         <img
+        <img
           className="w-[400px] rounded-lg mx-5"
           src="https://img.ws.mms.shopee.vn/16ab2a14e8c180e2d06db78820156010"
         />
-         <img
+        <img
           className="w-[400px] rounded-lg mx-5"
           src="https://m.media-amazon.com/images/I/616r3OmI6EL.jpg"
         />
-          <img
+        <img
           className="w-[400px] rounded-lg mx-5"
           src="https://chimcanhvietnam.vn/images/sanpham/slider/DSCN4558.JPG"
         />
